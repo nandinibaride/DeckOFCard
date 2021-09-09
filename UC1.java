00c2a6212bbfd7381676db3aac71d487890029d6
nandinibaride <88042024+nandinibaride@users.noreply.github.com>
2021-09-09 09:38:03 +0530

    Add: To create deck of card

--- UC1.java
+++ UC1.java
@@ -9,14 +9,14 @@ package deckofcard.com;
 	  */
 	 
 	public static void main(String[] args) {
+		
 		String[] suits = {"Diamond", "Heart", "Spades", "Club"};
         String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-
-        String[][] cardCombo = new String[4][13];
+        String[][] deck = new String[4][13];
        
           for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-              cardCombo[i][j] = suits[i] + rank[j];
+              deck[i][j] = suits[i] + rank[j];
             }
         }
             
